bf203dff13930feb16ee853d7336a02ccd11c35b
U-LAPTOP-CP46GUJ0\varshid <kabdi9232@gmail.com>
2021-11-12 21:21:30 -0600

    FASDFASDF

--- testAmogusName.py
+++ testAmogusName.py
@@ -4,8 +4,8 @@ device=usb.core.find(idVendor=0x413d,idProduct=0x2107)
 
 endpoint=device[0][(0,0)][0]
 
-if device.is_kernel_driver_active():
-    device.detach_kernel_driver()
+if device.is_kernel_driver_active(0):
+    device.detach_kernel_driver(0)
 
 device.set_configuration()
 
