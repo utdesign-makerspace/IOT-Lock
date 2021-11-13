7099649915f1e043af5384cd017becaf5aaf98a5
U-LAPTOP-CP46GUJ0\varshid <kabdi9232@gmail.com>
2021-11-12 21:18:35 -0600

    EERESRASERAS

--- testAmogusName.py
+++ testAmogusName.py
@@ -2,12 +2,12 @@ import usb.core
 
 device=usb.core.find(idVendor=0x413d,idProduct=0x2107)
 
-device.set_configuration()
-
 endpoint=device[0][(0,0)][0]
 
 if device.is_kernel_driver_active():
     device.detach_kernel_driver()
+
+device.set_configuration()
 
 while True:
     print(endpoint.bEndpointAddress, endpoint.wMaxPacketSize)
