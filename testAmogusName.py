be56ff1d4a5d02c36328939b1d8d7cb94764efc4
U-LAPTOP-CP46GUJ0\varshid <kabdi9232@gmail.com>
2021-11-12 21:12:04 -0600

    WHYYYYYYYYYYY

--- testAmogusName.py
+++ testAmogusName.py
@@ -1,15 +1,10 @@
 import usb.core
 
-dev=usb.core.find(idVendor=0x413d,idProduct=0x2107)
-ep=dev[0].interfaces()[0].endpoints()[0]
-i=dev[0].interfaces()[0].bInterfaceNumber
-dev.reset()
+device=usb.core.find(idVendor=0x413d,idProduct=0x2107)
 
-if dev.is_kernel_driver_active(i):
-    dev.detach_kernel_driver(i)
+device.set_configuration()
 
-dev.set_configuration()
-eaddr=ep.bEndpointAddress
+endpoint=device[0][(0,0)][0]
 
-r=dev.read(eaddr,1024)
-print(len(r))
+while True:
+    print(endpoint.bEndpointAddress, endpoint.wMaxPacketSize)
