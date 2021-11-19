f15fdf6df8ed6db50581ea36685ba561647cad64
U-LAPTOP-CP46GUJ0\varshid <kabdi9232@gmail.com>
2021-11-19 13:34:26 -0600

    Testing out example code

--- testAmogusName.py
+++ testAmogusName.py
@@ -3,21 +3,40 @@ import time
 
 device=usb.core.find(idVendor=0x413d,idProduct=0x2107)
 
-endpoint=device[0][(0,0)][0]
+if device is None:
+    raise ValueError('Device not found')
 
-if device.is_kernel_driver_active(0):
-    device.detach_kernel_driver(0)
+#endpoint=device[0][(0,0)][0]
+
+#if device.is_kernel_driver_active(0):
+#    device.detach_kernel_driver(0)
 
 device.set_configuration()
 
-data = None
-while True:
-    try:
-        data = device.read(endpoint.bEndpointAddress,
-                           endpoint.wMaxPacketSize)
-    except usb.core.USBError as e:
-        data = None
-        if e.args == ('Operation timed out',):
-            continue
-    print(data)
-    time.sleep(1)
+cfg = dev.get_active_configuration()
+intf = cfg[(0,0)]
+
+ep = usb.util.find_descriptor(
+    intf,
+    # match the first OUT endpoint
+    custom_match = \
+    lambda e: \
+        usb.util.endpoint_direction(e.bEndpointAddress) == \
+        usb.util.ENDPOINT_OUT)
+
+assert ep is not None
+
+# write the data
+ep.write('test')
+
+# data = None
+# while True:
+#     try:
+#         data = device.read(endpoint.bEndpointAddress,
+#                            endpoint.wMaxPacketSize)
+#     except usb.core.USBError as e:
+#         data = None
+#         if e.args == ('Operation timed out',):
+#             continue
+#     print(data)
+#     time.sleep(1)
