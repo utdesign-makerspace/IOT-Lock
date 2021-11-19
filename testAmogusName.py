d2febf9836d8708e2bd5d4e91ae973491059d137
U-LAPTOP-CP46GUJ0\varshid <kabdi9232@gmail.com>
2021-11-19 14:44:07 -0600

    haha I'm faster than cameron

--- testAmogusName.py
+++ testAmogusName.py
@@ -18,7 +18,7 @@ RxData = None
 while True:
     try:
         data = device.read(endpoint.bEndpointAddress,
-                           endpoint.wMaxPacketSize)
+                           endpoint.wMaxPacketSize).tobytes()
         RxData = ''.join([chr(x) for x in data])
     except usb.core.USBError as e:
         data = None
