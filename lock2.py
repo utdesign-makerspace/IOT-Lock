a6b1882b483aa55b1bbc8c5bf29b220d2ce2c207
varshid <kabdi9232@gmail.com>
2022-01-25 16:50:38 -0600

    Doint some testing

--- lock2.py
+++ lock2.py
@@ -3,7 +3,5 @@ import hid
 vid = 0x413d	# Change it for your device
 pid = 0x2107	# Change it for your device
 
-with hid.Device(vid, pid) as h:
-	print(f'Device manufacturer: {h.manufacturer}')
-	print(f'Product: {h.product}')
-	print(f'Serial Number: {h.serial}')
+while True:
+    print(hid.read(hid.Device(vid, pid)))
